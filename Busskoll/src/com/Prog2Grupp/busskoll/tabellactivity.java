612ee333e5801ed37e9e0c8b93551c715bb165ce
davidd95 <daviddiamant95@gmail.com>
2013-09-05 09:12:58 +0200

    update3
    swag

--- Busskoll/src/com/Prog2Grupp/busskoll/tabellactivity.java
+++ Busskoll/src/com/Prog2Grupp/busskoll/tabellactivity.java
@@ -11,7 +11,7 @@ public class tabellactivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
     	requestWindowFeature(Window.FEATURE_NO_TITLE);
     	super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.tabeller);
         
     }
 
