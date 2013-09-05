385ad6ec6cc36f6b08e1a5e4462f0b9c0b503b63
davidd95 <daviddiamant95@gmail.com>
2013-09-05 09:45:25 +0200

    update7
    sista för idag

--- Busskoll/gen/com/Prog2Grupp/busskoll/R.java
+++ Busskoll/gen/com/Prog2Grupp/busskoll/R.java
@@ -24,9 +24,11 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080003;
+        public static final int action_settings=0x7f080005;
         public static final int button1=0x7f080001;
         public static final int button2=0x7f080002;
+        public static final int button3=0x7f080003;
+        public static final int button4=0x7f080004;
         public static final int textView1=0x7f080000;
     }
     public static final class layout {
@@ -40,6 +42,10 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int buss1=0x7f050006;
+        public static final int buss2=0x7f050007;
+        public static final int buss3=0x7f050008;
+        public static final int buss4=0x7f050009;
         public static final int hello_world=0x7f050002;
         public static final int spåra=0x7f050005;
         public static final int tabeller=0x7f050004;
